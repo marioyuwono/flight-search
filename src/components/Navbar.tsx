e983728cef1426350f5ce13d75dce081e9ac9c19
Mario Yuwono <59589638+marioyuwono@users.noreply.github.com>
2026-01-21 13:45:05 +0700

    Update favicon and logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,13 @@ export const Navbar = () => {
         <Link href="/">
           <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
               <span>
-                {/* logo */}
+                <Image
+                  src="/img/logo.svg"
+                  width="32"
+                  alt="N"
+                  height="32"
+                  className="w-8"
+                />
               </span>
           </span>
         </Link>
